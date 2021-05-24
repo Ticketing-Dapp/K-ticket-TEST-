78269464c8ba1b2ebe460ace8a4813ef68a61613
seungbok <seungbok3240@gmail.com>
2021-05-24 20:01:49 +0900

    Create defaultTicket

--- contracts/TransferTrade.sol
+++ contracts/TransferTrade.sol
@@ -3,19 +3,21 @@ pragma solidity >=0.4.21 <0.7.0;
 contract TransferTrade {
 
     struct TicketInfo {
-        byte concertName;
-        byte day;   // 나중에 타입 다시 생각하기
-        byte time;  // 나중에 타입 다시 생각하기
+        string concertName;
+        string day;   // 나중에 타입 다시 생각하기
+        string time;  // 나중에 타입 다시 생각하기
         Seat seat;
         uint32 ticketPrice;
     }
 
     struct Seat {
-        byte typeOfSeat;
+        string typeOfSeat;
         uint32 seatNumber;
     }
 
     // 디폴트 티켓 생성
+    Seat public defaultSeat = Seat("VIP", 30);
+    TicketInfo defaultTicket = TicketInfo("EXO", "2021-06-18", "18:00", defaultSeat, 100000);
 
 
     // 변수
@@ -23,7 +25,7 @@ contract TransferTrade {
     /**
     * @dev 양도 신청 처리 함수
     */
-    function transferApplication() {
+    function transferApplication() public {
         // alreadyTransferringTicket을 확인
         // pay
         // changeTicketInfo
@@ -33,35 +35,35 @@ contract TransferTrade {
     /**
     * @dev 이미 거래중인 티켓인 경우
     */
-    function alreadyTransferringTicket() {
+    function alreadyTransferringTicket() internal {
 
     }
 
     /**
     * @dev 양수인의 계좌에서 티켓 가격을 받아 양도인에게 송금한다.
     */
-    function pay() {
+    function  pay() internal {
 
     }
 
     /**
     * @dev 양도거래가 끝난 티켓의 정보를 수정한다.
     */
-    function changeTicketInfo() {
+    function changeTicketInfo() internal {
 
     }
 
     /**
     * @dev 마이페이지에서 진행중인 양도거래 티켓을 보여준다.
     */
-    function getMyTransferringTicket() {
+    function getMyTransferringTicket() public {
 
     }
 
     /**
     * @dev 마이페이지에서 소유하고 있는 티켓을 보여준다.
     */
-    function getMyTicket() {
+    function getMyTicket() public {
 
     }
 }
